eb9e756122a83b6990b37a692f099614363144db
maximussmile <maximusgranddeveloper@gmail.com>
2016-12-01 18:14:41 +0200

    all categories readyy

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,7 +38,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-
+  config.i18n.fallbacks = true
   #for device gem
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
